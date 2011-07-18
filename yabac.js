ef43de2e7dd06b80f8b3f4d7d9ef2372fe876a86
mootoomeepo <mootoo.meepo@gmail.com>
2011-07-19 02:05:02 +0700

    init

---
+++ yabac.js
@@ -0,0 +1,15 @@
+/**
+ * Yet a better auto-complete
+ * (Apache License = Do whatever you want, but give me some credit)
+ * - Tanin Na Nakorn
+ */
+
+(function($) {
+	
+	$.fn.extend({
+		yabac_autocomplete: function(url, options) {
+	
+		}
+	});
+	
+})(jQuery);
